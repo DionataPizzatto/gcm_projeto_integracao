7c026717e138ef61c81e64b173aaa9f7c652c02d
Dionata Cintra Pizzatto <dionata_pizzatto@hotmail.com>
2014-04-06 21:19:29 -0300

    Update GCM_PROJETO_INTEGRACAO.java

--- src/gcm_projeto_integracao/GCM_PROJETO_INTEGRACAO.java
+++ src/gcm_projeto_integracao/GCM_PROJETO_INTEGRACAO.java
@@ -24,7 +24,7 @@ public class GCM_PROJETO_INTEGRACAO {
         GCM gcm;
         gcm = new GCM();
         try {
-            gcm.SendMSG("APA91bHn2NKIToOqNYMomoiauixom0O3mrhF6a95G2jIDx1UI8iNWGt1qyisJaltJPezk50zE5LRj7wxa31lt-cyjTe3VXcCDo1CVm0jTNuwVfUOBBaRdgdnwJ62g02gSlZf7UGUEYKoxbcO1JtwPs5UR8Xkmq8ES6zeb9Siff2rh5_ZZhAM3Y8", "MENSAGEM!");
+            gcm.SendMSG("fdgadgas-ae8ssafsjekjri4522345234423q-4wetayu43n25q4wa0se985r132qp4jtwaesd583we-cyjT2rh5_ZZhAM3Y8", "MENSAGEM!");
         } catch (UnsupportedEncodingException ex) {
             Logger.getLogger(GCM_PROJETO_INTEGRACAO.class.getName()).log(Level.SEVERE, null, ex);
         }
